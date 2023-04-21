fc4e32d0e3b7bc4d20309add7bd37e739ae9c0de
BonggonPark <121501385+ParkBonggon@users.noreply.github.com>
2023-04-21 18:37:13 +0900

    Update Firebase.js

--- src/api/Firebase.js
+++ src/api/Firebase.js
@@ -19,6 +19,7 @@ export  function login(){
     provider.setCustomParameters({
         prompt: "select_account",   
     });
+    console.log('dhktdjdy');
     signInWithPopup(auth, provider)
     .catch(console.error);
 }
